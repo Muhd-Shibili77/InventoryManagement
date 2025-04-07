581bba96218162c343a2c5292de5a43b051eb2db
shibili888 <shibilishanu888@gmail.com>
2025-04-07 18:03:37 +0530

    hh

--- client/src/services/AxiosInstance.js
+++ client/src/services/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: "https://inventorymanagement-8dfr.onrender.com/",
+  baseURL: "https://inventorymanagement-8dfr.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
