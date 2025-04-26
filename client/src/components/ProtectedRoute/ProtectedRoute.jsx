78844a114e040f4a9583a27dddf71d823eea3ea7
shibili888 <shibilishanu888@gmail.com>
2025-04-26 10:41:36 +0530

    loader activated

--- client/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import { refreshToken } from '../../services/AxiosInterceptor'; // Your refresh function
-
+import {Riple} from 'react-loading-indicators'
 const ProtectedRoute = ({ element }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
 
@@ -24,7 +24,7 @@ const ProtectedRoute = ({ element }) => {
             const newAccessToken = await refreshToken(); // Should return new access token
             if (newAccessToken) {
               localStorage.setItem('token', newAccessToken);
-              setIsAuthenticated(true);
+              setIsAuthenticated(true); // true
             } else {
               localStorage.removeItem('token');
               setIsAuthenticated(false);
@@ -34,7 +34,7 @@ const ProtectedRoute = ({ element }) => {
             setIsAuthenticated(false);
           }
         } else {
-          setIsAuthenticated(true);
+          setIsAuthenticated(true); // true
         }
       } catch (error) {
         localStorage.removeItem('token');
@@ -46,7 +46,12 @@ const ProtectedRoute = ({ element }) => {
   }, []);
 
   if (isAuthenticated === null) {
-    return <div>Loading...</div>; 
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Riple color={["#b2f5ea", "#81e6d9", "#4fd1c5", "#38b2ac"]} />
+      </div>
+    );
+    
   }
 
   if (!isAuthenticated) {
